ca731e999a2c0b7c4c3796ea3afd410a256c5c8d
Debian <developer@evilsaint-com.2aq2vg1ig1uejeufhqkji5sure.zx.internal.cloudapp.net>
2022-09-15 17:02:41 +0000

    trying out making pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,10 @@
 import React from "react"
 
-const HomePage = () => (
+const AboutPage = () => (
   <main>
-    <h1>Home</h1>
+    <h1>About</h1>
     <p>This is some test text</p>
   </main>
 )
 
-export default HomePage
+export default AboutPage
